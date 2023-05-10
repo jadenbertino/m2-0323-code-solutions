36ebf864d533ac2552e3580dc0bc9aec614aec96
Jaden Bertino <jaden@bertinofamily.com>
2023-05-09 20:23:45 -0700

    express-hello-world

---
+++ express-hello-world/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express()
+const PORT = 8080
+
+app.use((req, res) => {
+  console.log(req.method)
+  res.send('<h1>HOLA MUNDO !!</h2>')
+})
+
+app.listen(PORT, () => {
+  console.log(`Express server listening on port ${PORT}`)
+})
