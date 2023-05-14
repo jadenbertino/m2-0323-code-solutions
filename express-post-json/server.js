e67f624002abdaa5c2b1562887c353ef09123434
Jaden Bertino <jaden@bertinofamily.com>
2023-05-14 09:47:32 -0700

    express-post-json

--- express-post-json/server.js
+++ express-post-json/server.js
@@ -1,12 +1,23 @@
 import express from 'express';
 const app = express();
+
 const PORT = 8080;
+const GRADES_URL = '/api/grades';
 
 let nextID = 1;
-const grades = {};
+const grades = [];
 
-app.get('/api/grades', (req, res) => {
+app.use(express.json());
+
+app.get(GRADES_URL, (req, res) => {
   res.json(grades);
+});
+
+app.post(GRADES_URL, (req, res) => {
+  const gradesUpdate = { ...req.body, nextID };
+  grades.push(gradesUpdate);
+  res.status(201).json(gradesUpdate);
+  nextID++;
 });
 
 app.listen(PORT, () => {
