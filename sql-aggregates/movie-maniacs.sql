09b3fe88c3a74dcf29233fbe5e66b826b4f0dd8a
Jaden Bertino <jaden@bertinofamily.com>
2023-05-26 02:34:55 +0000

    split up firstName and lastName in movie-maniacs

--- sql-aggregates/movie-maniacs.sql
+++ sql-aggregates/movie-maniacs.sql
@@ -1,7 +1,8 @@
 -- List the first and last name of all customers, plus the total amount they've spent on rentals. Order them by total paid, descending.
 
 select
-  ("c"."firstName" || ' ' || "c"."lastName") as "customer",
+  "c"."firstName",
+  "c"."lastName",
   sum("p"."amount") as "totalSpent"
 from "customers" as "c"
 join "payments" as "p" using ("customerId")
